e01f45af0361ae8963611987df576eb2b7e75d8b
Jimmy Yang <codingExpert123@gmail.com>
2021-08-10 13:33:58 -0700

    Add non-Win32 version

--- sdl_rend_ex.c
+++ sdl_rend_ex.c
@@ -30,3 +30,41 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     SDL_Quit();
     return EXIT_SUCCESS;
 }
+/* without Win32 */
+/*
+// g++ t.cpp -o t -lSDL2
+
+#include <iostream>
+#include <stdlib.h>
+
+#define SDL_MAIN_HANDLED
+
+#include <SDL2/SDL.h>
+
+#define WINDOW_WIDTH 600
+
+int main() {
+    SDL_Event event;
+    SDL_Renderer *renderer;
+    SDL_Window *window;
+    int i;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_WIDTH, 0, &window, &renderer);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    for (i = 0; i < WINDOW_WIDTH; ++i)
+        SDL_RenderDrawPoint(renderer, i, i);
+    SDL_RenderPresent(renderer);
+    while (1) {
+        if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
+            break;
+    }
+    std::cout << "yo man\n";
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return EXIT_SUCCESS;
+}
+*/
