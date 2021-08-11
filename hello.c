9811dbd394bb2aa93f8405a400bc98bd47ec10aa
marethyu <codingexpert123@gmail.com>
2021-08-11 13:44:44 -0700

    Move to misc/

--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <windows.h>
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
-{
-	MessageBox(NULL, TEXT("Hello World!\n"), TEXT("Hey!"), MB_OK | MB_ICONWARNING);
-	
-	return 0;
-}
